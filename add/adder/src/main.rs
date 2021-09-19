d98002535eb4a720d951d941845151d4eda7cc8e
Tim H <timh1203@gmail.com>
2021-09-18 23:01:59 -0500

    Add add_two

--- add/adder/src/main.rs
+++ add/adder/src/main.rs
@@ -1,11 +1,18 @@
 use add_one;
+use add_two;
 use rand;
 
 fn main() {
     let num = 10;
+    // println!(
+    //     "Hello, world! {} plus one is {}!",
+    //     num,
+    //     add_one::add_one(num)
+    // )
+    
     println!(
-        "Hello, world! {} plus one is {}!",
+        "Hello, world! {} plus two is {}!",
         num,
-        add_one::add_one(num)
+        add_two::add_two(num)
     )
 }
